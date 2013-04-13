6f680fac083728143dcd9997b23f5b54bd018b6b
Nimrod Lahav <nimrod.lahav@gmail.com>
2013-04-13 23:20:33 +0300

    add temp index page

---
+++ index.php
@@ -0,0 +1,73 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <title>
+        </title>
+        <link rel="stylesheet" href="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+        <link rel="stylesheet" href="my.css" />
+        <script src="https://s3.amazonaws.com/codiqa-cdn/jquery-1.7.2.min.js">
+        </script>
+        <script src="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.js">
+        </script>
+        <script src="my.js">
+        </script>
+        <!-- User-generated css -->
+        <style>
+        </style>
+        <!-- User-generated js -->
+        <script>
+            try {
+
+    $(function() {
+
+    });
+
+  } catch (error) {
+    console.error("Your javascript has an error: " + error);
+  }
+        </script>
+    </head>
+    <body>
+        <!-- Home -->
+        <div data-role="page" id="page1">
+            <div data-theme="a" data-role="header">
+                <h3>
+                    Study Group
+                </h3>
+            </div>
+            <div data-role="content">
+                <h1 align='center'>
+                    Welcome to Study Group App
+                </h1>
+                <a href="#page1">
+                    <div style=" text-align:center">
+                        <img style="width: 160px; height: 200px" src="exam.png" />
+                    </div>
+                </a>
+                <a data-role="button" href="www.viewYourTests.com">
+                    view your tests
+                </a>
+                <a data-role="button" href="www.createNEWtests.com">
+                    create new test
+                </a>
+                <a data-role="button" href="www.testStatistics.com">
+                    tests statistics
+                </a>
+                <a data-role="button" href="www.aboutOrHelp.com">
+                    about / help
+                </a>
+            </div>
+            <div data-theme="a" data-role="footer" data-position="fixed">
+                <h3>
+                    Nimrod Lahav app
+                </h3>
+            </div>
+        </div>
+    </body>
+</html>
